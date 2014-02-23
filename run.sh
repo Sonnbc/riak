030ccd6726bf7e9a52af559a5205152ed1b0ede7
Son Nguyen <son@wirelessprvnat-172-16-241-98.near.illinois.edu>
2014-02-23 15:28:09 -0600

    update

--- run.sh
+++ run.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-REMOTE_DIR=/tmp
+REMOTE_DIR=/riak
+REL_DIR=/scratch/Confluence/riak/riak/riak-1.4.2/rel/riak
 HOSTS='./hosts'
 
 usage()
 {
-    echo "usage: $1 -u username [-d riak_directory|-s|-t]"
-    echo "actions: -d (deploy), -s (start), -t (shutdown)"
+    echo "usage: $1 -u username [-d|-s|-t|-c]"
+    echo "actions: -d (deploy), -s (start), -t (shutdown), -c (change shell to bash)"
     exit -1
 }
 
 deploy()
 {
+    #TODO: currently have to join by hand. need to automate
     echo "Deploy"
     i=0
     while read line; do 
@@ -20,15 +22,20 @@ deploy()
         ip=$(echo $line | awk '{print $2}')
         i=$((i+1))
         echo "Deploy riak #$i to host $host..."
-        rsync -az --delete $riak_dir $username@$host:$REMOTE_DIR
-        ssh $username@$host "
+        ssh -t $username@$ip "
+            sudo rm -rf $REMOTE_DIR;
+            sudo mkdir $REMOTE_DIR;
+            sudo chown -R $username: $REMOTE_DIR;
+            cp -rf $REL_DIR $REMOTE_DIR/;
             cd $REMOTE_DIR/riak;
             sed -i -e 's/127.0.0.1/$host/g' ./etc/vm.args;
             sed -i -e 's/127.0.0.1/$ip/g' ./etc/app.config;
-            ./bin/riak start; < /dev/null
-            if ! [ -z $prev_host ]; then
-                ./bin/riak-admin cluster join riak@$prev_host 
-            fi" < /dev/null
+            " < /dev/null
+            #ulimit -n 4096;
+            #./bin/riak start;
+            #if ! [ -z $prev_host ]; then
+            #    ./bin/riak-admin cluster join riak@$prev_host 
+            #fi" < /dev/null
 
     done < $HOSTS
 
@@ -36,18 +43,29 @@ deploy()
         exit 0
     fi
     
-    ssh $username@$host "
-        cd $REMOTE_DIR/riak;
-        ./bin/riak-admin cluster plan;
-        ./bin/riak-admin cluster commit;" < /dev/null
+    #ssh $username@$host "
+    #    cd $REMOTE_DIR/riak;
+    #    ./bin/riak-admin cluster plan;
+    #    ./bin/riak-admin cluster commit;" < /dev/null
 }
 
+
+# changeshell()
+# {
+#     echo "Change shell"
+#     while read line; do
+#        host=$(echo $line | awk '{print $2}')
+#        printf 'Change shell to bash at %s...' $host
+#        printf "ssh $username@$host" 
+#        ssh -t $username@$host "chsh -s /bin/bash;" < /dev/null
+#     done < $HOSTS
+# }
 
 start()
 {
     echo "Start"
     while read line; do
-       host=$(echo $line | awk '{print $1}')
+       host=$(echo $line | awk '{print $2}')
        printf 'Start riak at %s...' $host 
        ssh $username@$host "
            cd $REMOTE_DIR/riak;
@@ -60,7 +78,7 @@ shutdown()
 {
     echo "Shutdown"
     while read line; do
-       host=$(echo $line | awk '{print $1}')
+       host=$(echo $line | awk '{print $2}')
        printf 'Shut down riak at %s...' $host 
        ssh $username@$host "
            cd $REMOTE_DIR/riak;
@@ -68,13 +86,13 @@ shutdown()
     done < $HOSTS
 }
 
-while getopts "u:d:st" opt; do
+while getopts "u:dstc" opt; do
     case "$opt" in
         u)  username=$OPTARG;;
-        d)  riak_dir=$OPTARG;
-            action='deploy';;
+        d)  action='deploy';;
         s)  action='start';;
         t)  action='shutdown';;
+        c)  action='changeshell';;
     esac
 done    
 
